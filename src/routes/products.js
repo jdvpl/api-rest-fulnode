fe9b815016d381285e44c2d5f588c45842517f98
JDVPL <juanda554242@gmail.com>
2021-11-25 13:21:07 -0500

    colocando data fake y colocarla en el navegador

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,11 @@
 const express=require('express');
-
-
+// importando la data fake
+const ds= require("../datasource/data");
 const routers=express.Router();
 
 routers.get('/',function (req, res) {
-    res.send("Productos")
+    const products=ds.fetchProducts();
+    res.status(200).json(products);
 });
 
 module.exports=routers
