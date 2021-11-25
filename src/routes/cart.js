a2ae1721b2ccbd467c16b1e0d59e8a85cea67229
JDVPL <juanda554242@gmail.com>
2021-11-25 16:12:29 -0500

    obteniendo cart por su id

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -13,9 +13,21 @@ routers.post('/', function(req, res){
 
    res.status(201).location(`/cart/${id}`).send()
 })
-
+// obtener todos
 routers.get("/", function(req, res){
     res.status(200).json(ds.fetchCartItems());
+})
+// obtener por id
+routers.get("/:id", function(req, res){
+
+    const cartid=req.params.id;
+    const cart=ds.fetchCartItemById(cartid);
+
+    if(cart != undefined){
+        res.status(200).json(cart);
+    }else{
+        res.status(200).json(`No se encontro canasta con el id ${cartid}`)
+    }
 })
 
 module.exports=routers;
